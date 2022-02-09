40691d3d969d09fd702ce63dcff732c23ac0ce14
mostafa266 <mostafa.abdo.ch12@gmail.com>
2022-02-09 13:09:59 +0200

    test

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 
 document.querySelector('.toggle')
-        .addEventListener('click', function() {
-  this.classList.toggle('activate');
-  $('.menu-mobile').fadeToggle(500);
+    .addEventListener('click', function() {
+      this.classList.toggle('activate');
+      $('.menu-mobile').fadeToggle(500);
 });
+
+// GSAP
+
